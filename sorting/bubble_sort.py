0bd2322e81ab464c40250fa55ef5e2121aee652a
Anurag-gg <dsanurag520@gmail.com>
2022-04-25 18:20:44 +0530

    bug fixes

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,29 +1,39 @@
 import dearpygui.dearpygui as dpg
 import time
 
+# sort logic
+def bubble_sort(sender, app_data, user_data):
+    x_value, y_value = user_data
+    dpg.configure_item("click", show=False)
 
-#ending the visual
-def end():
-    dpg.delete_item("sort_window")
-    dpg.delete_item("click_end")
-
-#sort logic
-def bubble_sort(sender , app_data , user_data):
-    x_value , y_value = user_data
-    dpg.delete_item("click")
     for i in range(len(y_value) - 1):
         for j in range(len(y_value) - 1 - i):
             if y_value[j] > y_value[j + 1]:
                 y_value[j], y_value[j + 1] = y_value[j + 1], y_value[j]
-            time.sleep(0.01)
-            dpg.delete_item("graphs")
-            dpg.add_bar_series(
-                x_value, y_value, parent="y_axis",tag="graphs"
-            )
-            dpg.bind_item_theme("graphs", "plot_theme")
-    dpg.bind_item_theme("graphs", "plot_theme_final")
+            time.sleep(0.05)
 
-    #mouse/keyboard input to end the visualization
-    with dpg.handler_registry(tag="click_end"):
-        dpg.add_mouse_down_handler(callback=end)
-        dpg.add_key_down_handler(callback=end)
+            if dpg.does_item_exist("graphs"):
+                dpg.delete_item("graphs")
+            if dpg.does_item_exist("highlight"):
+                dpg.delete_item("highlight")
+
+            dpg.add_bar_series(
+                [x for x in x_value if x not in (x_value[j], x_value[j + 1])],
+                [y for y in y_value if y not in (y_value[j], y_value[j + 1])],
+                parent="y_axis",
+                tag="graphs",
+            )
+            dpg.add_bar_series(
+                [x_value[j], x_value[j + 1]],
+                [y_value[j], y_value[j + 1]],
+                parent="y_axis",
+                tag="highlight",
+            )
+
+            dpg.bind_item_theme("graphs", "plot_theme")
+            dpg.bind_item_theme("highlight", "plot_theme_highlight")
+
+    dpg.bind_item_theme("graphs", "plot_theme_final")
+    dpg.bind_item_theme("highlight", "plot_theme_final")
+
+    dpg.configure_item("click_end", show=True)
